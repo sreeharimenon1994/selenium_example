66245f4e529c36df4c1a84fb69e615c01606c099
Balu Reghunathan <braghunathan@trl.co.uk>
2020-03-31 01:37:06 +0100

    headless version

--- bbc.py
+++ bbc.py
@@ -1,10 +1,21 @@
 from selenium import webdriver
-path= 'C:\\Users\\DELL\\Downloads\\sree\\selenium\\drivers\\chromedriver.exe'
-driver = webdriver.Chrome(path)
 
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+# use for headless option
+# chrome_options.add_argument("--headless")
+# use it while using headless option
+# chrome_options.add_argument("--window-size=1920x1080")
+
+
+driver_path = 'C:\\Users\\DELL\\Downloads\\sree\\selenium\\drivers\\chromedriver.exe'
 url = 'https://www.bbc.co.uk/news/health-52096049' 
 
+driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=driver_path)
+driver.maximize_window()
+
 driver.get(url)
+driver.implicitly_wait(1)
 
 title = driver.find_element_by_class_name('story-body__h1')
 
